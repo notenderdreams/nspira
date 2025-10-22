4c3a7174cdd1334897c986f0a363c63dffbc7e33
notenderdreams <enderdreams@outlook.com>
2025-10-21 23:30:34 -0700

    Impl:Remove Command

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -1,14 +1,13 @@
-pub fn run(id: &u32) {
-    // 1. Fetch project
-    // let project = crate::db::get_project_by_id(id);
+pub fn run(id: i32) ->anyhow::Result<()>{
+    
+    if !crate::db::project_exists(id)? {
+        eprintln!("No project found with ID {}", id);
+        return anyhow::Ok(());
+    }
 
-    // if project.is_none() {
-    //     eprintln!("No project found with ID {}", id);
-    //     return;
-    // }
-
-    //  2. Delete from DB
-    // crate::db::remove_project(id);
+    crate::db::remove_project(id)?;
 
     println!("Removed project with ID {}", id);
+
+    anyhow::Ok(())
 }
