7b8d4b435437841ee8aeb229cf0484a9f630230e
Nahian <168405797+notenderdreams@users.noreply.github.com>
2025-10-23 03:45:53 -0700

    Feat: Display total freed space

--- src/commands/clean.rs
+++ src/commands/clean.rs
@@ -1,29 +1,31 @@
 use anyhow::Result;
-use crate::utils::logger::{ success, task};
+use crate::utils::{get_dir_size, human_readable_size};
+use crate::utils::logger::{success, task};
 
 pub fn run(id: Option<i32>) -> Result<()> {
     match id {
         Some(pid) => {
             task(&format!("Cleaning cache for project ID {}", pid));
-            clean_cache(pid)?;
-            success("Cleaned!");
+            let size = clean_cache(pid)?;
+            success(&format!("{} freed from the disk",human_readable_size(size)));
         }
         None => {
-            task("Cleaning All Projects");
+            task("Cleaning all caches");
             let projects = crate::db::get_all_projects()?;
+            let mut size = 0;
             for project in projects {
-                clean_cache(project.id)?;
+               size += clean_cache(project.id)?;
             }
-            success("Cleaned!");
+            success(&format!("{} freed from the disk",human_readable_size(size)));
         }
     }
     Ok(())
 }
 
-fn clean_cache(pid: i32) -> Result<()> {
+fn clean_cache(pid: i32) -> Result<u64> {
     let project = crate::db::get_project_by_id(pid)?.expect("No such project found with that id");
-
+    let size = get_dir_size(&project.cache_dir);
     crate::utils::clean_dir(&project.cache_dir)?;
     crate::db::update_last_cleaned(pid)?;
-    Ok(())
+    Ok(size)
 }
